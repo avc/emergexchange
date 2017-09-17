b88f23d6eef7fc755b6387355e74034e7dab8fca
Anthony <non.zero@gmail.com>
2017-09-16 17:58:40 -0700

    Add URL for good list.

---
+++ trade/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from trade.views import GoodList
+
+urlpatterns = [
+    url(r'^list', GoodList.as_view()),
+]
--- trade/views.py
+++ trade/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.core.paginator import (
+    EmptyPage, PageNotAnInteger, Paginator)
+from .models import *
 
-# Create your views here.
+class GoodList(View):
+    page_arg = 'page'
+    paginate_by = 50
+    template_name = 'trade/good_list.html'
+
+    def get(self, request, page_number=1):
+        good_list = Good.objects.all()
+        paginator = Paginator(good_list, self.paginate_by)
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = reverse('good_list',
+                args=[page.previous_page_number()])
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = reverse('good_list',
+                args=[page.next_page_number()])
+        else:
+            next_url = None
+        context = {
+            'is_paginated': page.has_other_pages(),
+            'paginator': paginator,
+            'prev_url': prev_url,
+            'next_url': next_url,
+            'tag_list': page}
+        return render(request, self.template_name, context)
+
